2a32537c140c1a783e684e99c00531b3b9336edd
Dod-Annie <936166900@qq.com>
2018-10-25 23:42:02 +0800

    init

---
+++ alien_invasion.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    # init
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(screen)
+
+    # set background color
+    bg_color = (230, 230, 230)
+
+    while True:
+        # 监听键盘鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # 每次循环都重绘屏幕
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+
+run_game()
