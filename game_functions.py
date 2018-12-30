6ed23a155a375b77b3d8b584a6759046dca1d380
Dod-Annie <936166900@qq.com>
2018-12-30 22:18:51 +0800

    alien fleet direction

--- game_functions.py
+++ game_functions.py
@@ -106,3 +106,24 @@ def get_number_rows(ai_settings, ship_height, alien_height):
                         (3 * alien_height) - ship_height)
     number_rows = int(availble_space_y / (2 * alien_height))
     return number_rows
+
+
+def check_fleet_edges(ai_settings, aliens):
+    '''有外星人到达边缘是采取相应的措施'''
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    '''将整群外星人下移'''
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    '''检查是否有外星人位于屏幕边缘,更新外星人群中所有外星人的位置'''
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
