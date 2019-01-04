9442414759d44a05566f14d6335fef6d914ee831
Dod-Annie <936166900@qq.com>
2019-01-04 23:31:11 +0800

    add Class Button

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 from settings import Settings
 from ship import Ship
 from alien import Alien
@@ -16,6 +17,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, 'Play')
 
     # 创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
@@ -41,7 +45,8 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship,
+                         aliens, bullets, play_button)
 
 
 run_game()
