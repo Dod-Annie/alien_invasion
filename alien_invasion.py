ba692a5742d3be6bd7d8ff71e5f938562faf17dc
Dod-Annie <936166900@qq.com>
2019-02-10 21:45:54 +0800

    add scoreboard

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ from button import Button
 from settings import Settings
 from ship import Ship
 from alien import Alien
+from game_stats import GameStats
+from scoreboard import Scoreboard
 import game_functions as gf
 
 
@@ -36,6 +38,10 @@ def run_game():
     # 创建外星人群
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
+    # 创建存储游戏统计信息的实例，并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
     # 开始游戏主循环
     while True:
         gf.check_events(ai_settings, screen, stats,
@@ -46,7 +52,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, stats, ship,
+        gf.update_screen(ai_settings, screen, stats, sb, ship,
                          aliens, bullets, play_button)
 
 
--- game_functions.py
+++ game_functions.py
@@ -70,7 +70,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         ship.center_ship()
 
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     ''' 更新屏幕图像 并切换新屏幕'''
     ''' 每次循环都重绘屏幕 '''
     screen.fill(ai_settings.bg_color)
@@ -80,6 +80,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # 显示得分
+    sb.show_score()
 
     # 如果游戏处于非活动状态，就绘制Play按钮
     if not stats.game_active:
